9a39d85548b0a2ae8a90718b9dbcab52737a340e
MarcosRecio <33786384+MarcosRecio@users.noreply.github.com>
2018-10-03 20:47:47 +0100

    Add files via upload

---
+++ CommonDenominator.py
@@ -0,0 +1,3 @@
+# Common multiples of i and j in a range (0,...,n)
+print(filter(lambda x, i=int(input('1st denominator')), j=int(input('2nd denominator')): x%i==0 and x%j==0, range(int(input('Enter a number')))))
+ 
